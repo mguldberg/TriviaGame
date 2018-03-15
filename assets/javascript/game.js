75bde10391b5ed62918890e3961f247519c399d0
mguldberg <guldberg@sbcglobal.net>
2018-03-15 14:18:39 -0500

    completed Option Two: Advanced Assignment (Timed Questions)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,23 +25,9 @@ var numberTimeouts = 0;
 // ==============================================================================
 //
 
-// remove class from an HTML element
-function showElement(elementName) {
-    console.log(elementName);
-    $(elementName).removeClass("d-none");
-}
-
-// remove class from an HTML element
-function changeTextInDisplay(elementName, textToChange) {
-    console.log(elementName);
-    $(elementName).text(textToChange);
-
-}
-
 // display the next question
 function nextQuestion() {
-
-
+    //setup the div by changing the question answer choices
     console.log(questionsObj["Q" + currentQuestion].question);
     console.log(questionsObj["Q" + currentQuestion].choices[0]);
     console.log(questionsObj["Q" + currentQuestion].choices[1]);
@@ -63,24 +49,30 @@ function gameInit() {
     $("#results-container").hide();
 }
 
+//function to show the answer
 function showAnswer(reason) {
 
     console.log(questionsObj["Q" + currentQuestion].correctAnswer);
     console.log("checking to see if correct or not")
 
+    // setup var so the answer indexing isn't so hard to read
     var correctAnswerIndex = questionsObj["Q" + currentQuestion].correctAnswer
+    
+    //did we get here because the user didn't answer in time - setup the out to display 
     if (reason == "timeout") {
         $("#correct-not-correct-text").text("Too late!");
         //The correct answer was ...
         $("#question-answer-response").text("The correct answer was " + questionsObj["Q" + currentQuestion].choices[correctAnswerIndex] + ".");
 
     }
+    //else if check if user answer was correct - setup the output of 'correct' if true
     else if (answer == questionsObj["Q" + currentQuestion].correctAnswer) {
         questionsObj["Q" + currentQuestion].userAnswer = true;
         $("#correct-not-correct-text").text("That was correct!");
         $("#question-answer-response").text("");
 
     }
+    //else we got here because they answered incorrectly - set up the text to tell the user that
     else {
         console.log(" The correct answer was: " + questionsObj["Q" + currentQuestion].choices[correctAnswerIndex]);
         questionsObj["Q" + currentQuestion].answer = false;
@@ -90,8 +82,13 @@ function showAnswer(reason) {
 
     }
 
+    // set up the image in the div
     $("#answer-img").html('<img src="' + questionsObj["Q" + currentQuestion].answerImage + ' ">');
+    
+    // hide the question
     $(".questions-and-answers").hide();
+
+    // show the answer with image
     $("#question-answer-container").show();
 
 
@@ -105,25 +102,26 @@ function checkIfQuestionTimeIsExpired() {
     gameTimerCounter--; // deduct 1 second
     console.log(gameTimerCounter);
 
-    // write timer change to the screen
-    $("#countdown-time").text(gameTimerCounter);
+    // // write timer change to the screen
+    // $("#countdown-time").text(gameTimerCounter);
 
     //did we detect a timeout waiting for an answer?
     //  if yes - count the timeout and display the answer then got to next question
     if (gameTimerCounter == 0) {
         console.log("inside Game Time =0");
+        console.log("there was a timeout");
+
         // DONE: Use clearInterval to stop the count here and set the clock to not be running.
         clearInterval(gameTimer);
 
-
         //increment number of timeouts
         numberTimeouts++;
-
-        console.log("there was a timeout");
-
+        
+        //display that time ran out
         showAnswer("timeout");
 
-        answerTimer = setInterval(checkIfAnswerTimeIsExpired, 1000);
+        // show the results/answers and image for 5 seconds
+        answerTimer = setInterval(checkIfAnswerTimeIsExpired, 5000);
 
     }
 
@@ -139,7 +137,7 @@ function checkIfAnswerTimeIsExpired() {
     currentQuestion++;
 
     //check if game is over
-    if (currentQuestion == 20) {
+    if (currentQuestion == 4) {
         // tabulate the results
         for (i = 0; i < currentQuestion; i++) {
 
@@ -155,11 +153,14 @@ function checkIfAnswerTimeIsExpired() {
             }
         }
 
-        $("#results").text("You got " + numberCorrect + "correct.  Of the ones you got wrong, "
-            + numberTimeouts + "were because you didn't answer quickly enough.");
+        //write the results of the game to the div
+        $("#results").text("You got " + numberCorrect + " out 4 correct.  Of the ones you got wrong, "
+            + numberTimeouts + " were because you didn't answer quickly enough.");
 
-
+        // hide the questions
         $("#question-answer-container").hide();
+
+        // show the results/answers and image for 5 seconds
         $("#results-container").show();
 
     }
@@ -182,164 +183,37 @@ function checkIfAnswerTimeIsExpired() {
 
 var questionsObj = {
     Q0: {
-        question: "What is the color of the sky?",
-        choices: ["blue", "green", "orange", "gold"],
+        question: "What is the distance between the pitcher and homeplate?",
+        choices: ["60 ft 6 in", "59 ft 11 in", "90 ft", "60 ft 10 in"],
         correctAnswer: 0,
-        answerImage: "assets/images/inspector-gadget.jpg",
+        answerImage: "assets/images/metropolitan_stadium_12.gif",
         userAnswer: false
     },
     Q1: {
-        question: "What is the color of the sky?4",
-        choices: ["blue", "green", "orange", "gold"],
+        question: "Who was the winnder of the 1987 World Series?",
+        choices: ["Yankees", "Twins", "Cardinals", "Cubs"],
         correctAnswer: 1,
-        answerImage: "assets/images/u-of-m-block-m.png",
+        answerImage: "assets/images/twins-hqdefault.jpg",
         userAnswer: false
 
     },
     Q2: {
-        question: "What is the color of the sky?8",
-        choices: ["blue", "green", "orange", "gold"],
+        question: "What was the name of the Montreal MLB baseball team?",
+        choices: ["Barons", "Colt .45s", "Senators", "Expos"],
         correctAnswer: 3,
-        answerImage: "assets/images/djdkfj.jpg",
+        answerImage: "assets/images/EXPOS_LOGO.png",
         userAnswer: false
 
     },
     Q3: {
-        question: "What is the color of the sky?12",
-        choices: ["blue", "green", "orange", "gold"],
+        question: "What was the name of the MLB commissioner for most of the early 80s?",
+        choices: ["George W. Bush", "A. Bartlett Giamatti", "Bowie Kuhn", "Peter Ueberroth"],
         correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
+        answerImage: "assets/images/84389154.jpg",
         userAnswer: false
 
     },
-    Q4: {
-        question: "What is the color of the sky?16",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
 
-    },
-    Q5: {
-        question: "What is the color of the sky?20",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q6: {
-        question: "What is the color of the sky?24",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q7: {
-        question: "What is the color of the sky?28",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q8: {
-        question: "What is the color of the sky?32",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q9: {
-        question: "What is the color of the sky?36",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q10: {
-        question: "What is the color of the sky?40",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q11: {
-        question: "What is the color of the sky?44",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q12: {
-        question: "What is the color of the sky?48",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q13: {
-        question: "What is the color of the sky?52",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q14: {
-        question: "What is the color of the sky?56",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q15: {
-        question: "What is the color of the sky?60",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q16: {
-        question: "What is the color of the sky?64",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q17: {
-        question: "What is the color of the sky?68",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q18: {
-        question: "What is the color of the sky?72",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
-    Q19: {
-        question: "What is the color of the sky?76",
-        choices: ["blue", "green", "orange", "gold"],
-        correctAnswer: 2,
-        answerImage: "assets/images/djdkfj.jpg",
-        userAnswer: false
-
-    },
 
 };
 
@@ -357,11 +231,13 @@ $(document).ready(function () {
         //set up intial game screen - explain the rules
         // Hide start button
         // Show first question and start the timer
-        $("#start-container").hide();
+        $("#start-").hide();
         $("#results-container").hide();
 
         $("#questions-container").show();
         $("#questions-and-answers").show();
+        $("#start-button").hide();
+
 
         nextQuestion();
         gameTimer = setInterval(checkIfQuestionTimeIsExpired, 1000);
@@ -375,7 +251,7 @@ $(document).ready(function () {
 
     // after each possible outcome
     //      - Current currentQuestion++
-    //      - Check if currentQuestion == 20
+    //      - Check if currentQuestion == 4
     //          - end the game and show how many the person got wrong/right/timout   
     //      - change Q to next Q
 
@@ -416,7 +292,8 @@ $(document).ready(function () {
 
         showAnswer();
 
-        answerTimer = setInterval(checkIfAnswerTimeIsExpired, 1000);
+        // show the results/answers and image for 5 seconds
+        answerTimer = setInterval(checkIfAnswerTimeIsExpired, 5000);
 
 
     });
@@ -429,10 +306,14 @@ $(document).ready(function () {
         console.log("reset on click handler function");
 
         //reset the game
-        gameInit();
+        // gameInit();
+        $("#question-answer-container").hide();
         $("#results-container").hide();
-        
+
         $("#start-container").show();
+        $("#start-button").hide();
+        $(".questions-and-answers").show();
+
         //reset gameTimer
         gameTimerCounter = 25;
 
@@ -449,6 +330,9 @@ $(document).ready(function () {
 
         //reset current question to 0 - aka the beginning
         currentQuestion = 0;
+
+        nextQuestion();
+        gameTimer = setInterval(checkIfQuestionTimeIsExpired, 1000);
 
     });
 
